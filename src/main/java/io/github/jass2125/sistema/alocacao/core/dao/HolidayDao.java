ae1b0363b4674e33ecf0cc940eb69de5bc51254d
Anderson Souza <jair_anderson_bs@hotmail.com>
2016-02-16 23:27:04 -0300

    inserção de feriado

--- src/main/java/io/github/jass2125/sistema/alocacao/core/dao/HolidayDao.java
+++ src/main/java/io/github/jass2125/sistema/alocacao/core/dao/HolidayDao.java
@@ -12,10 +12,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 
 /**
  * Classe que implementa os DAO's de feriado
